5f665f050ab7fc779c51deb3df110b63f2a7a10f
Agnes <agnes.mccall5@gmail.com>
2024-05-26 21:56:28 -0400

    Add boilerplate and model for pet

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, redirect, request
+from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db, Pet
+
+app = Flask(__name__)
+app.debug = True
+app.secret_key = 'secret'
+toolbar = DebugToolbarExtension(app)
+
+app.app_context().push()
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///adopt'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# app.config['SQLALCHEMY_ECHO'] = True
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+connect_db(app)
+# db.create_all()
